1a2cb641b344f56c713d6e586db5b1db45cb2d1f
mikebmiller@hotmail.com <mbmiller@ra.rockwell.com>
2019-05-25 19:56:21 -0500

    Add missing files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          "bsa-red": "#CE1126",
+          "bsa-blue": "#003F87",
+          "bsa-brown": "#996633",
+          "bsa-yellow": "#FFCC00",
+          "bsa-green": "#006B3F",
+          "bsa-tan": "#d6cebd"
+        }
+      }
+    },
+    variants: {},
+    plugins: []
+  };
